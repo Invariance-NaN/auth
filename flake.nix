ddd8813c2ac619b9ab2b7973359dcfd5c8406ff1
Quinn Flavel <quinnydf@yahoo.ca>
2025-03-19 11:17:50 +0000

    te

--- flake.nix
+++ flake.nix
@@ -7,12 +7,11 @@
     inputs.systems.follows = "systems";
   };
 
-  outputs =
-    { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
+  outputs = { nixpkgs, flake-utils, self, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        self-packages = self.packages.${system};
       in
       {
         devShells.default = pkgs.mkShell { packages = [
@@ -20,6 +19,40 @@
           pkgs.nodejs
           pkgs.pnpm
         ]; };
+
+        apps.default = {
+          type = "app";
+          program = "${self-packages.run}/bin/run";
+        };
+
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "auth-server";
+          version = "0.1.0";
+          src = ./.;
+          buildInputs = [ pkgs.nodejs pkgs.pnpm.configHook ];
+
+          buildPhase = ''
+            pnpm run build
+          '';
+
+          installPhase = ''
+            mkdir -p $out
+            cp package.json $out
+            cp -r node_modules $out
+            cp -r public $out
+            cp -r src $out
+          '';
+
+          pnpmDeps = pkgs.pnpm.fetchDeps {
+            inherit (self-packages.default) pname version src;
+            hash = "sha256-2saQOvsFCQc6EwIpKODGjjlUi8jPzZ15MnR294Lij6g=";
+          };
+        };
+
+        packages.run = pkgs.writeShellScriptBin "auth-server" ''
+          cd ${self-packages.default}
+          ${pkgs.pnpm}/bin/pnpm run serve "$@"
+        '';
       }
     );
 }
--- src/data.ts
+++ src/data.ts
@@ -1,14 +1,13 @@
 import sqlite3 from "better-sqlite3";
 import { verify } from "argon2";
+import { dbPath } from "./config";
 
-export const db = sqlite3("data.sqlite");
+export const db = sqlite3(dbPath);
 
 db.exec(`
 	CREATE TABLE IF NOT EXISTS "config" ( "key" varchar NOT NULL PRIMARY KEY, "value" varchar );
 	CREATE TABLE IF NOT EXISTS "keys" ( "id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, "name" varchar NOT NULL, "key" varchar NOT NULL );
 `);
-
-// 	CREATE TABLE IF NOT EXISTS "sessions" ( "session_id" varchar NOT NULL PRIMARY KEY, "expires" integer NOT NULL );
 
 const getKeysStmt = db.prepare("SELECT id, name, key FROM keys");
 export const getKeys = async function(): Promise<Array<{ id: number, name: string, key: string }>> {
@@ -20,18 +19,3 @@ export const checkPassword = async function(password: string): Promise<boolean> 
 	const { value: digest } = getDigestStmt.get() as { value: string };
 	return verify(digest, password);
 }
-
-// const checkSessionStmt = db.prepare("SELECT 1 FROM sessions WHERE session_id = @sessionID AND expires > @now")
-// const cleanSesssionsStmt = db.prepare("DELETE FROM sessions WHERE expires <= @now");
-// export const checkSession = async function(sessionID: string): Promise<boolean> {
-// 	const res = checkSessionStmt.get({ sessionID, now: Date.now() }) as { "1": 1 } | undefined
-// 	cleanSesssionsStmt.run({ now: Date.now() });
-// 	return !!res && Boolean(res[1]);
-// }
-
-// const registerSessionStmt = db.prepare("INSERT INTO sessions (session_id, expires) VALUES (@sessionID, @expires);")
-// export const registerSession = async function(expires: Date): Promise<string> {
-// 	const sessionID = uuidv4();
-// 	registerSessionStmt.run({ sessionID, expires: expires.getTime() })
-// 	return sessionID;
-// }
