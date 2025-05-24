a4b9217ca174fecafee290f350c187370b4bb310
Quinn Flavel <quinnydf@yahoo.ca>
2025-05-24 04:58:54 +0000

    node2nix

--- flake.nix
+++ flake.nix
@@ -1,25 +1,43 @@
 {
-  description = "Auth app";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  inputs.systems.url = "github:nix-systems/default";
-  inputs.flake-utils = {
-    url = "github:numtide/flake-utils";
-    inputs.systems.follows = "systems";
+  description = "A simple MFA app";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
+    };
   };
 
-  outputs =
-    { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        nodeDependencies = (pkgs.callPackage ./node-composition.nix { pkgs = pkgs; }).nodeDependencies;
       in
       {
         devShells.default = pkgs.mkShell { packages = [
           pkgs.bashInteractive
           pkgs.nodejs
-          pkgs.pnpm
+          pkgs.node2nix
         ]; };
+
+        packages.default = pkgs.stdenv.mkDerivation {
+          name = "auth";
+          src = ./.;
+          buildInputs = [ pkgs.nodejs ];
+          buildPhase = ''
+            ln -s ${nodeDependencies}/lib/node_modules ./node_modules
+            export PATH="${nodeDependencies}/bin:$PATH"
+
+            npm run build
+            cp -r node_modules $out/
+            cp -r public $out/
+            cp -r src $out/
+            cp -r package.json $out/
+          '';
+        };
       }
     );
 }
